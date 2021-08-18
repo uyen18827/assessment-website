49e5f422c697d76fcc2160cd5cbc9c3054e21115
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-08-19 04:08:21 +0700

    func: edited hamburger menu

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,5 @@ let hamburger = document.querySelector('.hamburger');
 let menu = document.querySelector('.navbar');
 
 hamburger!.addEventListener('click', () => {
-    hamburger!.classList.toggle('isactive');
     menu!.classList.toggle('active');
-
 });
-
-// alert('heeeewwoo uwu')
